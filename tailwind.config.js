4d1e0a165609598826e9d22b82ac460952b6fd1b
flippette <quangdatle2006@outlook.com>
2024-03-07 15:51:02 +0700

    build pipeline fixes, drop win32 support for now

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   content: ["./content/**/*.md", "./templates/*.html"],
+  theme: {
+    fontFamily: {
+      laira: ["Laira", "ui-sans-serif"],
+      montserrat: ["Montserrat", "ui-sans-serif"],
+      lozungcaps: ["Lozung Caps", "ui-sans-serif"],
+      prime: ["Prime", "ui-sans-serif"],
+    },
+  },
 };
